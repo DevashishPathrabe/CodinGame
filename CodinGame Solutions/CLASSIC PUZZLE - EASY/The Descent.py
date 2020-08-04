450d4652e1a8aa8eec270728ca4ad763f26a7eb5
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-04 17:01:05 +0530

    The Descent
    Here is my The Descent Solution...

---
+++ CodinGame Solutions/CLASSIC PUZZLE - EASY/The Descent.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+# game loop
+while True:
+    max = 0
+    max_i = 0
+    for i in range(8):
+        mountain_h = int(input())  # represents the height of one mountain.
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+        if(mountain_h > max):
+            max = mountain_h
+            max_i = i
+    print(max_i)
+    # The index of the mountain to fire on.
