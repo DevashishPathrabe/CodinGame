f2a37d4abc47282566bf8f8c9a03f25cda6b4d74
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-05 17:06:41 +0530

    Mars Lander - Episode 1
    Here is my Mars Lander - Episode 1 Solution...

--- CodinGame Solutions/CLASSIC PUZZLE - EASY/Mars Lander - Episode 1.py
+++ CodinGame Solutions/CLASSIC PUZZLE - EASY/Mars Lander - Episode 1.py
@@ -3,21 +3,13 @@ import math
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-landX_prev = landY_prev = 0
-landing_start = 0
-landing_end = 7000
-landing_height = 0
-surfaceN = int(input())  # the number of points used to draw the surface of Mars.
 
-for i in range(surfaceN):
+surface_n = int(input())  # the number of points used to draw the surface of Mars.
+for i in range(surface_n):
     # land_x: X coordinate of a surface point. (0 to 6999)
     # land_y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-    landX, landY = [int(j) for j in input().split()]
-    if(landX_prev != 0 and landY_prev == landY):
-        landing_height = landY
-        landing_start = landX_prev
-        landing_end = landX
-    landX_prev, landY_prev = landX, landY
+    land_x, land_y = [int(j) for j in input().split()]
+
 # game loop
 while True:
     # h_speed: the horizontal speed (in m/s), can be negative.
@@ -29,14 +21,9 @@ while True:
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-    if(landing_start < x < landing_end):
-        dist = y - landing_height
-        if v_speed < -35:
-            power = 4
-        else:
-            power = 3
-        if(dist > 1200 and v_speed > -45):
-            power = int(power*(1-dist/3000))
+    if(v_speed <= -40):
+        print("0 4")
+    else:
+        print("0 0")
 
     # 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
-    print("0 " + str(power))
