a83640e74abfd2b38a5fb556bab74c50885e1f39
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-10-31 19:09:47 +0530

    Credit Card Verifier (Luhn’s algorithm)

---
+++ CodinGame Solutions/CLASSIC PUZZLE - EASY/Credit Card Verifier (Luhn’s algorithm).py
@@ -0,0 +1,31 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+def helper(number) -> int:
+    res = 0
+    for digit in str(number):
+        res += int(digit)
+    return res
+n = int(input())
+for i in range(n):
+    card = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    card = card.replace(' ', '')
+    cardRevert = card[::-1]
+    sum = 0
+    isOdd = True
+    for number in cardRevert:
+        if(isOdd):
+            sum += int(number)
+        else:
+            sum += helper(2 * int(number))
+        isOdd = not isOdd
+    if(sum%10 == 0):
+        print("YES")
+    else:
+        print("NO")
+
